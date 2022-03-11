95d19648d9cd1a9361bfb9eea5559d1059b003fe
Julia Schneider <julia_schneider_mail@posteo.de>
2022-03-11 16:14:30 +0100

    add increment & decrement function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import CurrentGame from './pages/CurrentGame';
+import { useState } from 'react';
 
 function App() {
-  return <CurrentGame />;
+  const [count, setCount] = useState(0);
+
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+
+  function handleDecrement() {
+    setCount(count - 1);
+  }
+  return <CurrentGame onDecrement={handleDecrement} onIncrement={handleIncrement} score={count} />;
 }
 
 export default App;
--- src/pages/CurrentGame.js
+++ src/pages/CurrentGame.js
@@ -1,14 +1,14 @@
 import Player from '../components/Player';
 import Title from '../components/Title';
 
-export default function CurrentGame({ title, name, score }) {
+export default function CurrentGame({ onDecrement, onIncrement, score }) {
   return (
     <>
       <Title title="Name of Game" />
-      <Player name="Player 1" score="0" />
-      <Player name="Player 1" score="0" />
-      <Player name="Player 1" score="0" />
-      <Player name="Player 1" score="0" />
+      <Player onDecrement={onDecrement} onIncrement={onIncrement} name="Player 1" score={score} />
+      <Player name="Player 2" score="0" />
+      <Player name="Player 3" score="0" />
+      <Player name="Player 4" score="0" />
     </>
   );
 }
