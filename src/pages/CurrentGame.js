fb4d13d0bacaceba6b384b57f5522b779696c457
Julia Schneider <julia_schneider_mail@posteo.de>
2022-03-13 12:05:06 +0100

    Add Input.js

--- src/pages/CurrentGame.js
+++ src/pages/CurrentGame.js
@@ -1,5 +1,8 @@
 import Player from '../components/Player';
 import Title from '../components/Title';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import styled from 'styled-components';
 
 export default function CurrentGame({ onDecrement, onIncrement, score }) {
   return (
@@ -9,6 +12,18 @@ export default function CurrentGame({ onDecrement, onIncrement, score }) {
       <Player name="Player 2" score="0" />
       <Player name="Player 3" score="0" />
       <Player name="Player 4" score="0" />
+      <AddPlayer>
+        <Input placeholder="e.g. Harry Potter" description="Enter Name of Player:" />
+        <Button children="Add Player" />
+      </AddPlayer>
     </>
   );
 }
+
+const AddPlayer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: flex-end;
+  align-items: center;
+  gap: 1rem;
+`;
